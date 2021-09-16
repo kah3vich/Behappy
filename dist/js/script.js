57e3f6a77968cb891c344c38af18c0fccea3649a
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-09-16 18:44:47 +0300

    file

--- dist/js/script.js
+++ dist/js/script.js
@@ -490,6 +490,199 @@ $('.cat-collections__block-styled-2').on('click', function() {
 });
 
 //! --------------------------------------------------------------------------------------------
+
+
+//! Star оценка товара 
+
+$('#star-1').on('click', function() {
+    $('.model-catalog__card-info-reviews-stars-num').html('1');
+});
+$('#star-2').on('click', function() {
+    $('.model-catalog__card-info-reviews-stars-num').html('2');
+});
+$('#star-3').on('click', function() {
+    $('.model-catalog__card-info-reviews-stars-num').html('3');
+});
+$('#star-4').on('click', function() {
+    $('.model-catalog__card-info-reviews-stars-num').html('4');
+});
+$('#star-5').on('click', function() {
+    $('.model-catalog__card-info-reviews-stars-num').html('5');
+});
+
+
+//! --------------------------------------------------------------------------------------------
+
+//! Выпадающий список - способы оплаты 
+
+$('select#calendar').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+    $this.addClass('calendar-select-hidden'); 
+    $this.wrap('<div class="calendar-select"></div>');
+    $this.after('<div class="calendar-select-styled"></div>');
+
+    var $styledSelect = $this.next('div.calendar-select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+
+    var $list = $('<ul />', {
+        'class': 'calendar-select-options'
+    }).insertAfter($styledSelect);
+
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+
+    var $listItems = $list.children('li');
+
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.calendar-select-styled.calendar-active').not(this).each(function(){
+            $(this).removeClass('calendar-active').next('ul.calendar-select-options').hide();
+        });
+        $(this).toggleClass('calendar-active').next('ul.calendar-select-options').toggle();
+    });
+
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('calendar-active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+
+    $(document).click(function() {
+        $styledSelect.removeClass('calendar-active');
+        $list.hide();
+    });
+
+});
+
+//! --------------------------------------------------------------------------------------------
+
+
+//! Календарь скидок 
+
+const date = new Date();
+
+const renderCalendar = () => {
+    date.setDate(1);
+
+    const monthDays = document.querySelector(".days");
+
+    const lastDay = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDate();
+
+    const prevLastDay = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0
+    ).getDate();
+
+    const firstDayIndex = date.getDay();
+
+    console.log(lastDay)
+
+    const lastDayIndex = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDay();
+
+    const nextDays = 7 - lastDayIndex - 1;
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    
+
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+    document.querySelector(".date p").innerHTML = new Date().getFullYear();
+
+    let days = "";
+
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+        if (
+        i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth()
+        ) {
+        days += `<div class="today">${i}</div>`;
+        } else {
+        days += `<div>${i}</div>`;
+        }
+    }
+
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`;
+        monthDays.innerHTML = days;
+    }
+    };
+
+    document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+    });
+
+    document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+    });
+
+renderCalendar();
+
+// const mouthRu = $('.calendar h1').text()
+// if(mouthRu === 'January') {
+//     $('.calendar h1').html('Январь')
+// } else if(mouthRu === 'February') {
+//     $('.calendar h1').html('Февраль')
+// } else if(mouthRu === 'March') {
+//     $('.calendar h1').html('Март')
+// } else if(mouthRu === 'April') {
+//     $('.calendar h1').html('Апрель')
+// } else if(mouthRu === 'May') {
+//     $('.calendar h1').html('Май')
+// } else if(mouthRu === 'June') {
+//     $('.calendar h1').html('Июнь')
+// } else if(mouthRu === 'July') {
+//     $('.calendar h1').html('Июль')
+// } else if(mouthRu === 'August') {
+//     $('.calendar h1').html('Август')
+// } else if(mouthRu === 'September') {
+//     $('.calendar h1').html('Сентябрь')
+// } else if(mouthRu === 'October') {
+//     $('.calendar h1').html('Октябрь')
+// } else if(mouthRu === 'November') {
+//     $('.calendar h1').html('Ноябрь')
+// } else if(mouthRu === 'December') {
+//     $('.calendar h1').html('Декабрь')
+// } else {
+//     $('.calendar h1').html('')
+// }
+
+
+//! ------------------------------------------------------------------------------------
 //! Выпадающий список - сортировка 
 
 $('select#sort').each(function(){
