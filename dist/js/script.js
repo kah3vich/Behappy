111d96ccca40437b7eba0eb50c925e98b03d2a1b
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-09-17 15:45:22 +0300

    file

--- dist/js/script.js
+++ dist/js/script.js
@@ -130,55 +130,62 @@ $('select#pay').each(function(){
 });
 
 //! --------------------------------------------------------------------------------------------
-//! Выпадающий список выбора города 
-
-$('select#city').each(function(){
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
-
-    $this.addClass('c-select-hidden'); 
-    $this.wrap('<div class="c-select"></div>');
-    $this.after('<div class="c-select-styled"></div>');
-
-    var $styledSelect = $this.next('div.c-select-styled');
-    $styledSelect.text($this.children('option').eq(0).text());
-
-    var $list = $('<ul />', {
-        'class': 'c-select-options'
-    }).insertAfter($styledSelect);
-
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-    }
-
-    var $listItems = $list.children('li');
-
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select-styled.c-active').not(this).each(function(){
-            $(this).removeClass('c-active').next('ul.c-select-options').hide();
-        });
-        $(this).toggleClass('c-active').next('ul.c-select-options').toggle();
-    });
-
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('c-active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
-
-    $(document).click(function() {
-        $styledSelect.removeClass('c-active');
-        $list.hide();
-    });
-
+//! Открытие выпадающего списка 
+$('.i-header__block-model-city').on('click', function() {
+    $('.i-header__block-model-city-select').toggleClass('display-n')
+    $('.i-header__block-model-city-select').toggleClass('display-b')
 });
 
-//! --------------------------------------------------------------------------------------------
+//! Замена название города на выбранный
+$('#i-header-city-1').on('click', function() {
+    let i_header_city_1 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_1);
+});
+$('#i-header-city-2').on('click', function() {
+    let i_header_city_2 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_2);
+});
+$('#i-header-city-3').on('click', function() {
+    let i_header_city_3 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_3);
+});
+$('#i-header-city-4').on('click', function() {
+    let i_header_city_4 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_4);
+});
+$('#i-header-city-5').on('click', function() {
+    let i_header_city_5 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_5);
+});
+$('#i-header-city-6').on('click', function() {
+    let i_header_city_6 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_6);
+});
+$('#i-header-city-7').on('click', function() {
+    let i_header_city_7 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_7);
+});
+$('#i-header-city-8').on('click', function() {
+    let i_header_city_8 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_8);
+});
+$('#i-header-city-9').on('click', function() {
+    let i_header_city_9 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_9);
+});
+$('#i-header-city-10').on('click', function() {
+    let i_header_city_10 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_10);
+});
+$('#i-header-city-11').on('click', function() {
+    let i_header_city_11 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_11);
+});
+$('#i-header-city-12').on('click', function() {
+    let i_header_city_12 = $(this).text();
+    $('.i-header__block-model-city-title').html(i_header_city_12);
+});
+// to be continued...
 //! Меню 
 
 let t1 = new TimelineMax({ paused: true });
