8858dbcc061a30626ef76f9fa50254623947215d
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-09-20 23:23:53 +0300

    file

--- #src/js/quiz/quiz.js
+++ #src/js/quiz/quiz.js
@@ -1,4 +1,53 @@
+//! Передача значение в тег из input-range 
 
 $('#quiz-range').on('input',function () {
     $('#quiz-result-range').empty().append($(this).val());
 });
+
+//! ----------------------------------------------------------------
+
+
+//! Квиз кнопки для переключения 
+
+$('.quiz-content__block-whom-next').on('click', function() {
+    $('.quiz-content__block-whom').addClass('display-n');
+    $('.quiz-content__block-type').removeClass('display-n');
+});
+
+$('.quiz-content__block-type-prev').on('click', function() {
+    $('.quiz-content__block-whom').removeClass('display-n');
+    $('.quiz-content__block-type').addClass('display-n');
+});
+
+$('.quiz-content__block-type-next').on('click', function() {
+    $('.quiz-content__block-type').addClass('display-n');
+    $('.quiz-content__block-shade').removeClass('display-n');
+});
+
+$('.quiz-content__block-shade-prev').on('click', function() {
+    $('.quiz-content__block-type').removeClass('display-n');
+    $('.quiz-content__block-shade').addClass('display-n');
+});
+
+$('.quiz-content__block-shade-next').on('click', function() {
+    $('.quiz-content__block-color').removeClass('display-n');
+    $('.quiz-content__block-shade').addClass('display-n');
+});
+
+$('.quiz-content__block-color-prev').on('click', function() {
+    $('.quiz-content__block-color').addClass('display-n');
+    $('.quiz-content__block-shade').removeClass('display-n');
+});
+
+$('.quiz-content__block-color-next').on('click', function() {
+    $('.quiz-content__block-color').addClass('display-n');
+    $('.quiz-content__block-range').removeClass('display-n');
+});
+
+$('.quiz-content__block-range-prev').on('click', function() {
+    $('.quiz-content__block-color').removeClass('display-n');
+    $('.quiz-content__block-range').addClass('display-n');
+});
+
+
+//! ----------------------------------------------------------------
--- readme.md
+++ readme.md
@@ -7,3 +7,5 @@
 - ## New Detail: [click me](https://kah3vich.github.io/Behappy/dist/dnew.html)
 - ## Search: [click me](https://kah3vich.github.io/Behappy/dist/search.html)
 - ## 8 March: [click me](https://kah3vich.github.io/Behappy/dist/march.html)
+- ## Quiz: [click me](https://kah3vich.github.io/Behappy/dist/quiz.html)
+- ## 404: [click me](https://kah3vich.github.io/Behappy/dist/404.html)
